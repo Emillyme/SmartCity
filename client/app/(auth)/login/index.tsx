30ab775746a37e3c7286a13a831e63fb06c10808
emillyme <emill.ymello212@gmail.com>
2024-10-30 22:01:34 -0300

    Começando frontend (Shadcn já instalado)

---
+++ client/app/(auth)/login/index.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Eye, EyeOff, Github, Mail } from 'lucide-react';
+
+export default function Login() {
+  return (
+    <>
+        
+    </>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Login from "./(auth)/login";
+
+// import { useRouter } from "next/navigation";
+
+export default function Home() {
+  return (
+    <>
+      A Emilly é linda
+      <Login />
+    </>
+  );
+}
